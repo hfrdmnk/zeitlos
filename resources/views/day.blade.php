2f668912041f8d43a138657ad9fd1ba7a7529cd6
Dominik Hofer <hi@dominikhofer.me>
2024-08-07 14:15:27 +0200

    Basic day view with data passing created

---
+++ resources/views/day.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __('Your Day') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    @if (is_null($day->entry))
+                        <p>{{ __("You haven't written anything for today yet.") }}</p>
+                    @else
+                        <p>{{ __("Today's entry:") }}</p>
+                        <p>{{ $day->entry->story }}</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,28 @@
 <?php
 
+use App\Services\DayService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
 Route::view('/', 'welcome');
+
+Route::get('day', function (Request $request): View {
+    try {
+        $dateInput = $request->input('date', now()->toDateString());
+        $date = Carbon::createFromFormat('Y-m-d', $dateInput);
+    } catch (\Exception $e) {
+        $date = now()->startOfDay();
+        dd($date);
+    }
+    $dayService = new DayService();
+    $day = $dayService->createDay($date);
+
+    return view('day', compact('day'));
+})
+    ->middleware(['auth', 'verified'])
+    ->name('day');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
@@ -12,4 +32,4 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
