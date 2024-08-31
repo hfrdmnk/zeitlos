2bcd6bb777ce0df413e661db489660c3eda7eedc
Dominik Hofer <hi@dominikhofer.me>
2024-08-31 16:28:21 +0200

    Added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-		 './storage/framework/views/*.php',
-		 './resources/views/**/*.blade.php',
-		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
-	],
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './vendor/robsontenorio/mary/src/View/Components/**/*.php',
+    ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Switzer', ...defaultTheme.fontFamily.sans],
+                display: ['Instrument Serif', ...defaultTheme.fontFamily.serif],
             },
         },
     },
 
-    plugins: [
-		forms,
-		require("daisyui")
-	],
+    plugins: [forms, require('daisyui')],
 };
