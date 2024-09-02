d3c6d2a844024bac2105cb4128343f5d221683f5
Dominik Hofer <hi@dominikhofer.me>
2024-09-02 16:47:56 +0200

    User must verify email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $user = User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // $user = User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
         $startDate = Carbon::now()->subYears(3);
         $endDate = Carbon::now();
