33a248ed6fd88a9d5b79998d5a9a59814075902a
Dominik Hofer <hi@dominikhofer.me>
2024-09-04 17:06:36 +0200

    Added backup package

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Scheduling backups
+Schedule::command('backup:run --only-db')->daily()->at('04:00');
+Schedule::command('backup:clean')->daily()->at('04:30');
+Schedule::command('backup:monitor')->daily()->at('05:00');
