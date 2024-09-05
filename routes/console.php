e14f6c112a652ffca842e8ded3994b797564ff8b
Dominik Hofer <hi@dominikhofer.me>
2024-09-05 14:44:32 +0200

    Uncomment inspire command

--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
 
 // Scheduling backups
 Schedule::command('backup:run --only-db')->daily()->at('04:00');
